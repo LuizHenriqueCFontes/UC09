3026579e12d6a8d4b79f6281937d051cbe4d8cf1
Luiz <luizhenrique3781@gmail.com>
2026-05-06 21:33:26 -0300

    feat: complete payment manager

---
+++ payment-manager/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import PaymentForm from "../components/PaymentForm";
+import PaymentList from "../components/PaymentList";
+import { usePayments } from "../hooks/usePayments";
+
+export default function Home(){
+    const { payments, addPayment, removePayment, togglePaid } = usePayments();
+
+    return(
+        <div className="container">
+            <h2>Painel de Pagamentos</h2>
+
+            <PaymentForm onAdd={addPayment}/>
+
+            <PaymentList 
+                payments={payments}
+                onDelete={removePayment}
+                onToggle={togglePaid}
+            />
+        </div>
+
+        
+
+    );
+}
